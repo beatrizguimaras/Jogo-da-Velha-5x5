70d06c20550a0a116cdbc236f960292811e11a7f
beatrizguimaras <beatrizsthefaneguimaraes@gmail.com>
2025-05-31 08:18:01 -0300

    Add files via upload

---
+++ jogo-velha/script.js
@@ -0,0 +1,10 @@
+const cellElements = document.querySelectorAll("[data-cell]");
+
+let isCircleTurn;
+
+const handleClick = (e) => {
+  
+}
+for (const cell of cellEllements) {
+  cell.addEventListener("clik", handleClick, { once: true});
+}
